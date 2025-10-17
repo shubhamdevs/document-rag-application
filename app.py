2afc99f3017b65484dc27b79d6db54fa9eb585fa
SHUBHAM BISWAS <suvm.dev@gmail.com>
2025-10-17 21:06:59 +0530

    fix the file upload issue

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import dotenv
 import uuid
 
 
+# check if it's linux so it works on Streamlit Cloud
+if os.name == 'posix':
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 
 from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain.schema import HumanMessage, AIMessage
